2c56c8dfa9b6747c15a82e35a967b248f7a45405
Mahdi Heydari <mahdiheydari15879@gmail.com>
2022-10-15 09:42:28 +0330

    get user country name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 import "./styles/styles.js";
 
 function App() {
+  const [currentLocation, setCurrentLocation] = useState({});
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    const location = await axios.get("https://ipapi.co/json");
+    setCurrentLocation(location.data);
+  };
+
   return <div className="App"></div>;
 }
 
